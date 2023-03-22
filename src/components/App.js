8e0aef80a49b8694b0cfa6066213d927fa38d4b5
Ajaykarki13 <ajaymum698@gmail.com>
2023-03-22 20:09:01 +0530

    soln

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,14 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key='relativeList'>
+                <li key='relativeListItem1'>ram</li>
+                <li key='relativeListItem2'>charan</li>
+                <li key='relativeListItem3'>allu</li>
+                <li key='relativeListItem4'>arjun</li>
+                <li key='relativeListItem5'>ntr</li>
+
+               </ol>
     </div>
   )
 }
